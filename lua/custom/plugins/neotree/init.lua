97b177352a3862b752a2c446658ed28634515b5e
miutamihai <miuta.mihai@gmail.com>
2023-10-30 15:55:14 +0200

    [master] Split neotree config

--- lua/custom/plugins/neotree/init.lua
+++ lua/custom/plugins/neotree/init.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
-  dependencies = require 'custom.plugins.neotree.dependencies'
+  dependencies = require 'custom.plugins.neotree.dependencies',
+  config = require 'custom.plugins.neotree.config'
 }
