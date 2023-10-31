104b7b4a4e8ad17f8a399cfa0ac6f1e1438c9c85
miutamihai <miuta.mihai@gmail.com>
2023-10-31 15:29:07 +0200

    [master] Add lspkind nvim

--- init.lua
+++ init.lua
@@ -108,7 +108,27 @@ require('lazy').setup({
 
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
+      'onsails/lspkind.nvim',
     },
+    config = function()
+      local lspkind = require 'lspkind'
+
+      require('cmp').setup {
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol',       -- show only symbol annotations
+            maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(_, vim_item)
+              return vim_item
+            end
+          })
+        }
+      }
+    end
   },
 
   -- Useful plugin to show you pending keybinds.
