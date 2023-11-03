b89582102607091e4fdf7824d4fce7234bc7e8ad
miutamihai <miuta.mihai@gmail.com>
2023-11-03 10:05:40 +0200

    [master] Remove neovide transparency

--- lua/custom/setup/neovide/init.lua
+++ lua/custom/setup/neovide/init.lua
@@ -4,11 +4,11 @@ return function()
     vim.o.guifont = 'DaddyTimeMono Nerd Font Propo:h18'
 
     -- Blur
-    vim.g.neovide_floating_blur_amount_x = 4.0
-    vim.g.neovide_floating_blur_amount_y = 4.0
+    -- vim.g.neovide_floating_blur_amount_x = 4.0
+    -- vim.g.neovide_floating_blur_amount_y = 4.0
 
     -- Transparency
-    vim.g.neovide_transparency = 0.6
+    -- vim.g.neovide_transparency = 0.6
 
     -- Theme
     vim.g.neovide_theme = 'dark'
