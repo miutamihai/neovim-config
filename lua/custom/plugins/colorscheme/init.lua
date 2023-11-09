ca9cf97cde1ea47ecb64ca2f311bdeef2e8b08c6
miutamihai <miuta.mihai@gmail.com>
2023-11-09 17:16:35 +0200

    [master] Edit colorscheme

--- lua/custom/plugins/colorscheme/init.lua
+++ lua/custom/plugins/colorscheme/init.lua
@@ -3,46 +3,11 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  opts = {
-    flavour = "mocha",
-    background = { light = "latte", dark = "mocha" },
-    dim_inactive = {
-      enabled = false,
-      shade = "dark",
-      percentage = 0.15,
-    },
-    transparent_background = true,
-    show_end_of_buffer = false,
-    term_colors = true,
-    compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-    styles = {
-      comments = { "italic" },
-      properties = { "italic" },
-      functions = { "bold" },
-      keywords = { "italic" },
-      operators = { "bold" },
-      conditionals = { "bold" },
-      loops = { "bold" },
-      booleans = { "bold", "italic" },
-      numbers = {},
-      types = {},
-      strings = {},
-      variables = {},
-    },
-    integrations = {
-      treesitter = true,
-      cmp = true,
-      dap = { enabled = true, enable_ui = true },
-      dashboard = true,
-      gitsigns = true,
-      mason = true,
-      neotree = { enabled = true, show_root = true, transparent_panel = false },
-      noice = true,
-      treesitter_context = true,
-    },
-    color_overrides = {},
-  },
+  opts = {},
   config = function()
+    local opts = require "custom.plugins.colorscheme.opts"
+    require("catppuccin").setup(opts)
+
     vim.cmd [[colorscheme catppuccin]]
   end,
 
