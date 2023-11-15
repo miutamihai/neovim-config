593480ebe5e4003a3325809ba38d9f2df2b4f66d
miutamihai <miuta.mihai@gmail.com>
2023-11-15 13:43:41 +0200

    [master] Add git diff shortcut

--- lua/custom/setup/keymaps.lua
+++ lua/custom/setup/keymaps.lua
@@ -1,8 +1,17 @@
+-- Mihai Explains: This file handlers _most_ shortcuts
+
 return function()
+  -- Neotree
   vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>Neotree reveal<cr>',
     { noremap = true, silent = true, desc = "Toggle Explorer" })
 
+  -- Buffers
   vim.api.nvim_set_keymap('n', '<leader>bo', '<cmd>BufOnly<cr>',
     { noremap = true, silent = true, desc = "Close all buffers except current one" }
+  )
+
+  -- Git
+  vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>lua require("gitsigns").diffthis() <cr>',
+    { noremap = true, silent = true, desc = "View Git diff" }
   )
 end
