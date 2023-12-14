01c973dbf033c5f0d17f04b5bf9ba54437545605
mihaimiuta <mihaimiuta@proton.me>
2023-12-14 11:57:50 +0200

    [master] Add nix support

--- lua/custom/setup/lsp/servers.lua
+++ lua/custom/setup/lsp/servers.lua
@@ -54,4 +54,7 @@ return {
 
   -- F#
   fsautocomplete = {},
+
+  -- Nix
+  rnix = {},
 }
--- lua/custom/setup/treesitter/languages.lua
+++ lua/custom/setup/treesitter/languages.lua
@@ -1,4 +1,4 @@
 return {
   'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-  'bash', 'elm', 'elixir', 'heex', 'html', 'clojure', 'svelte', 'nim', 'zig', 'ocaml', 'wgsl'
+  'bash', 'elm', 'elixir', 'heex', 'html', 'clojure', 'svelte', 'nim', 'zig', 'ocaml', 'wgsl', 'nix'
 }
