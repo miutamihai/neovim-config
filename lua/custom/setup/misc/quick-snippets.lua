f40e398302e0f889c2a723d4e723f82cab793ea2
mihaimiuta <mihaimiuta@proton.me>
2024-02-13 11:50:22 +0200

    [master] Change i to a

--- lua/custom/setup/misc/quick-snippets.lua
+++ lua/custom/setup/misc/quick-snippets.lua
@@ -2,5 +2,5 @@
 
 return function()
   vim.keymap.set('n', '<leader>pp', 'aPULA ')
-  vim.keymap.set('n', '<leader>ph', 'ipulaLuiHristos')
+  vim.keymap.set('n', '<leader>ph', 'apulaLuiHristos')
 end
