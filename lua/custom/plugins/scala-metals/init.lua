68fa062bdc2e04ff77bd55d628b17862d77de95c
mihaimiuta <mihaimiuta@proton.me>
2024-02-20 12:40:43 +0200

    [master] Autostart scala metals on file entry

--- lua/custom/plugins/scala-metals/init.lua
+++ lua/custom/plugins/scala-metals/init.lua
@@ -5,5 +5,15 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'mfussenegger/nvim-dap',
-  }
+  },
+  config = function(self, metals_config)
+    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = self.ft,
+      callback = function()
+        require("metals").initialize_or_attach(metals_config)
+      end,
+      group = nvim_metals_group,
+    })
+  end
 }
