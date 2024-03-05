af5c5e862bb40a001c729c34a2c6f6cd3ef618cc
mihaimiuta <mihaimiuta@proton.me>
2024-03-05 08:15:08 +0200

    [master] Setup swift

--- lua/custom/setup/autocommands/init.lua
+++ lua/custom/setup/autocommands/init.lua
@@ -1,6 +1,7 @@
 -- Mihai Explains: this one sets up all autocmds
 
 return function()
+  -- This one sets code lens
   vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
     pattern = { "*" },
     group = vim.api.nvim_create_augroup('CodeLensRefresh', { clear = true }),
@@ -14,5 +15,24 @@ return function()
         end
       end
     end
+  })
+
+  -- This one's for swift LSP setup
+  local swift_lsp = vim.api.nvim_create_augroup("swift_lsp", { clear = true })
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "swift" },
+    callback = function()
+      local root_dir = vim.fs.dirname(vim.fs.find({
+        "Package.swift",
+        ".git",
+      }, { upward = true })[1])
+      local client = vim.lsp.start({
+        name = "sourcekit-lsp",
+        cmd = { "sourcekit-lsp" },
+        root_dir = root_dir,
+      })
+      vim.lsp.buf_attach_client(0, client)
+    end,
+    group = swift_lsp,
   })
 end
