e2739730882ce1fdbc3178d6e435e259e86bbbc3
mihaimiuta <mihaimiuta@proton.me>
2024-03-05 11:01:24 +0200

    [master] Change theme again

--- init.lua
+++ init.lua
@@ -182,8 +182,8 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'everforest',
-        -- theme = 'nightfox',
+        -- theme = 'everforest',
+        theme = 'nightfox',
         -- theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
--- lua/custom/plugins/colorscheme/init.lua
+++ lua/custom/plugins/colorscheme/init.lua
@@ -1,5 +1,5 @@
 -- Mihai Explains: Think this one's obvious
 
-local theme = require 'custom.plugins.colorscheme.everforest'
+local theme = require 'custom.plugins.colorscheme.nightfox'
 
 return theme
