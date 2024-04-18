57791397353824f0d6594dba9cb696639696bb2c
mihaimiuta <mihaimiuta@proton.me>
2024-04-18 15:31:21 +0300

    [master] Add current file name snippet

--- lua/custom/setup/misc/quick-snippets.lua
+++ lua/custom/setup/misc/quick-snippets.lua
@@ -1,6 +1,14 @@
 -- Mihai Explains: This one stores quick snippets
 
 return function()
-  vim.keymap.set('n', '<leader>pp', 'aPULA ')
-  vim.keymap.set('n', '<leader>ph', 'apulaLuiHristos')
+  vim.api.nvim_set_keymap('n', '<leader>pp', '<cmd>aPULA <cr>',
+    { noremap = true, silent = true, desc = "[P]ut: 'PULA'" })
+
+  vim.api.nvim_set_keymap('n', '<leader>ph', '<cmd>apulaLuiHristos<cr>',
+    { noremap = true, silent = true, desc = "[P]ut: 'PulaLuiHristos'" })
+
+  vim.keymap.set('n', '<leader>pf', function()
+    local filename = vim.fn.expand('%:t:r')
+    vim.api.nvim_put({ filename }, '', true, true)
+  end, { noremap = true, silent = true, desc = "[P]ut: Current File Name" })
 end
