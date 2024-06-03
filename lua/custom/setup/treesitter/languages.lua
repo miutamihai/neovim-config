f1fbe22d99592c86cb52ff799a6aa05dc64ade31
mihaimiuta <mihaimiuta@proton.me>
2024-06-03 09:59:01 +0300

    [master] Remove rescript treesitter parser

--- lua/custom/setup/treesitter/languages.lua
+++ lua/custom/setup/treesitter/languages.lua
@@ -1,5 +1,5 @@
 return {
   'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'terraform',
-  'bash', 'elm', 'elixir', 'heex', 'html', 'svelte', 'nix', 'templ', 'haskell', 'erlang', 'ocaml', 'proto', 'rescript',
+  'bash', 'elm', 'elixir', 'heex', 'html', 'svelte', 'nix', 'templ', 'haskell', 'erlang', 'ocaml', 'proto',
   'swift', 'gleam', 'kotlin', 'purescript', 'dhall'
 }
